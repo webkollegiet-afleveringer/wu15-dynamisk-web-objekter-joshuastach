d18fc38e15508059bfaab11b1c1cc7be293a1e60
U2479227@rts-365.dk <U2479227@rts-365.dk>
2026-02-03 09:26:24 +0100

    Opgave 2 done

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
+// let person ={
+//     firstname: "Bro",
+//     lastname: "Zo",
+//     age: 19,
+//     hometown: "BrobroTown",
+//     pet: "Glings",
+//     petname: "Gløggert"
+// }
+
+// let person1 = {};
+// person1["firstName"] = "Zozo";
+// person1["lastName"] = "Brobro";
+
+// console.log(person1.firstName)
+
+
+
+// console.log(person.firstname);
+// console.log(person.lastname);
+// console.log(person.pet);
+// console.log(person.hometown);
+
+
 let person ={
     firstname: "Bro",
     lastname: "Zo",
+    single: true,
     age: 19,
+    hobbies: ["Art", "2D Artist/2D Animator", "3D Designer", "Musican"],
     hometown: "BrobroTown",
-    pet: "Glings",
-    petname: "Gløggert"
+    pet:{
+        type: "Beanie",
+        name: "Robert Nymand"
+    },
 }
 
-let person1 = {};
-person1["firstName"] = "Zozo";
-person1["lastName"] = "Brobro";
+person.hobbies.forEach(function(hobby) {
+    console.log(hobby);
+       
+})
+console.log(person.pet.type);
+console.log(person.pet.name);
 
-console.log(person1.firstName)
-
-
-
-console.log(person.firstname);
-console.log(person.lastname);
-console.log(person.pet);
-console.log(person.hometown);
-
-
-
-
+console.log(person);
